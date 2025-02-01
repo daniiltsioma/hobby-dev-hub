8d1f44e677b293ac45f0405625cf55fb4255ff29
dtsiomapdx <dev.dtsioma@gmail.com>
2025-01-31 16:40:02 -0800

    tokens saved in cookies

--- frontend/app/api/login/route.ts
+++ frontend/app/api/login/route.ts
@@ -1,4 +1,5 @@
 import { NextRequest } from "next/server";
+import { cookies } from "next/headers";
 
 const GITHUB_APP_CLIENT_ID = process.env.GITHUB_APP_CLIENT_ID || "";
 const GITHUB_APP_CLIENT_SECRET = process.env.GITHUB_APP_CLIENT_SECRET || "";
@@ -27,8 +28,36 @@ export async function GET(request: NextRequest) {
 
     const responseText = await response.text();
 
-    return new Response(
-        `Successfully authenticated with code ${githubCode}. Github response ${responseText}`,
+    const tokenData: any = {};
+
+    responseText.split("&").map((str) => {
+        const [key, value] = str.split("=");
+        tokenData[key] = value;
+    });
+
+    const cookieStore = await cookies();
+
+    cookieStore.set({
+        name: "accessToken",
+        value: tokenData.access_token,
+        expires: new Date(new Date() + tokenData.expires_in),
+        httpOnly: true,
+        path: "/",
+    });
+
+    cookieStore.set({
+        name: "refreshToken",
+        value: tokenData.refresh_token,
+        expires: new Date(new Date() + tokenData.refresh_token_expires_in),
+        httpOnly: true,
+        path: "/",
+    });
+
+    return Response.json(
+        {
+            accessToken: cookieStore.get("accessToken"),
+            refreshToken: cookieStore.get("refreshToken"),
+        },
         {
             status: 200,
         }
