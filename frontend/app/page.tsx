7df6976c58f1612a96302579e1ab5d315c34dbe6
dtsiomapdx <dev.dtsioma@gmail.com>
2025-02-01 09:47:50 -0800

    logout added

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -8,7 +8,15 @@ export default async function Home() {
             <div className="text-2xl font-bold">Hobby Dev Hub</div>
             <div>
                 {user ? (
-                    <>Welcome, {user.data.login}</>
+                    <div className="flex items-center">
+                        Welcome, {user.data.login}
+                        <a
+                            href="/api/logout"
+                            className="border hover:bg-red-100 border-red-600 text-red-600 py-1 px-4 rounded-md ml-4"
+                        >
+                            Logout
+                        </a>
+                    </div>
                 ) : (
                     <a
                         href={`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_APP_CLIENT_ID}`}
