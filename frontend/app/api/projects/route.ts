7897ec080b145757a75ba55e7cf69b9df45a062f
dtsiomapdx <dev.dtsioma@gmail.com>
2025-02-01 10:31:29 -0800

    projects list added

---
+++ frontend/app/api/projects/route.ts
@@ -0,0 +1,33 @@
+import { NextRequest } from "next/server";
+
+export interface Project {
+    id: number;
+    title: string;
+    description: string;
+    githubRepoURL: string;
+}
+
+const projects: Project[] = [
+    {
+        id: 1,
+        title: "Project 1",
+        description: "Description of project 1",
+        githubRepoURL: "https://github.com/daniiltsioma/pizzashop",
+    },
+    {
+        id: 2,
+        title: "Project 2",
+        description: "Description of project 2",
+        githubRepoURL: "https://github.com/daniiltsioma/pizzashop",
+    },
+    {
+        id: 3,
+        title: "Project 3",
+        description: "Description of project 3",
+        githubRepoURL: "https://github.com/daniiltsioma/pizzashop",
+    },
+];
+
+export async function GET() {
+    return Response.json(projects);
+}
