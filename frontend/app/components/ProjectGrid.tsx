448a2d7fc5ee35209df8c0df6796b4ace7329701
Nicholai Venetucci <npv2@pdx.edu>
2025-02-05 23:23:54 -0800

    Add components folder and ProjectGrid component

---
+++ frontend/app/components/ProjectGrid.tsx
@@ -0,0 +1,7 @@
+export default function ProjectGrid() {
+  return (
+    <div className="container mx-auto p-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6"></div>
+    </div>
+  );
+}
