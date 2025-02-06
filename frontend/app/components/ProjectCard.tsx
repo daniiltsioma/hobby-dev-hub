6dc6260a5055fea34dfa15e4c0118da276a52eff
Nicholai Venetucci <npv2@pdx.edu>
2025-02-06 13:40:51 -0800

    Add an object prop of type Project to the ProjectCard component

--- frontend/app/components/ProjectCard.tsx
+++ frontend/app/components/ProjectCard.tsx
@@ -1,13 +1,15 @@
-export default function ProjectCard() {
+import { Project } from "../api/projects/route";
+
+export default function ProjectCard({ project }: { project: Project }) {
   return (
     <div className="bg-[#0d1117] text-white border border-[#3d444d] rounded-lg">
       <div className="p-6">
-        <h3 className="text-lg font-semibold mb-2">Project Title</h3>
-        <p className="text-gray-400 mb-2">
-          This is an example description for the project. You can add more text
-          here or customize it.
-        </p>
-        <a href="#" className="text-blue-500 hover:underline">
+        <h3 className="text-lg font-semibold mb-2">{project.title}</h3>
+        <p className="text-gray-400 mb-2">{project.description}</p>
+        <a
+          href={project.githubRepoURL}
+          className="text-blue-500 hover:underline"
+        >
           Learn More
         </a>
       </div>
