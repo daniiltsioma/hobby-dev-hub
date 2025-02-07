7b7ed360df08a4400f86e7a44389c006fc41ee28
Nicholai Venetucci <npv2@pdx.edu>
2025-02-06 23:57:03 -0800

    Fetch projects in ProjectGrid and map each to a ProjectCard

--- frontend/app/components/ProjectGrid.tsx
+++ frontend/app/components/ProjectGrid.tsx
@@ -1,7 +1,19 @@
-export default function ProjectGrid() {
+import { Project } from "../api/projects/route";
+import ProjectCard from "./ProjectCard";
+
+const HOST_URL = process.env.HOST_URL;
+
+export default async function ProjectGrid() {
+  const response = await fetch(`${HOST_URL}/api/projects`);
+  const projects: Project[] = await response.json();
+
   return (
     <div className="container mx-auto p-4">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6"></div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {projects.map((project) => (
+          <ProjectCard project={project} key={project.id} />
+        ))}
+      </div>
     </div>
   );
 }
