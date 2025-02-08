a3cfc70239e2ebff7ca0a5d8738b06f760f69470
Nicholai Venetucci <npv2@pdx.edu>
2025-02-07 23:51:03 -0800

    Add Link for ProjectCard title and minor style changes

--- frontend/app/components/ProjectCard.tsx
+++ frontend/app/components/ProjectCard.tsx
@@ -1,20 +1,23 @@
+import Link from "next/link";
 import { Project } from "../api/projects/route";
 
 export default function ProjectCard({ project }: { project: Project }) {
   return (
     <div className="bg-[#0d1117] text-white border border-[#3d444d] rounded-lg">
       <div className="p-6">
-        <h3 className="text-lg font-semibold mb-2">{project.title}</h3>
+        <h3 className="text-lg font-semibold hover:underline mb-2">
+          <Link href={`/projects/${project.id}`}>{project.title}</Link>
+        </h3>
         <p className="text-gray-400 mb-2">{project.description}</p>
         <a
           href={project.githubRepoURL}
           className="text-blue-500 hover:underline"
         >
-          Learn More
+          View on GitHub
         </a>
       </div>
       <div className="flex justify-end bg-[#151b23] border-t border-[#3d444d] rounded-b-lg p-4">
-        <button className="bg-blue-800 hover:bg-blue-900 font-medium border border-[#3d444d] rounded-md py-2 px-4">
+        <button className="bg-blue-800 hover:bg-blue-900 font-medium rounded-md py-2 px-4">
           Apply
         </button>
       </div>
