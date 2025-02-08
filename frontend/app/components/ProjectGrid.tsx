a90a0cc1eff14c40bac8b65a464dc6c201367c92
Nicholai Venetucci <npv2@pdx.edu>
2025-02-07 19:39:26 -0800

    Change ProjectGrid to accept a prop of type Project[]

--- frontend/app/components/ProjectGrid.tsx
+++ frontend/app/components/ProjectGrid.tsx
@@ -1,12 +1,11 @@
 import { Project } from "../api/projects/route";
 import ProjectCard from "./ProjectCard";
 
-const HOST_URL = process.env.HOST_URL;
-
-export default async function ProjectGrid() {
-  const response = await fetch(`${HOST_URL}/api/projects`);
-  const projects: Project[] = await response.json();
-
+export default async function ProjectGrid({
+  projects,
+}: {
+  projects: Project[];
+}) {
   return (
     <div className="container mx-auto p-4">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
