c2be47e2bad759cd0867e877a376d645c5cdbc00
Nicholai Venetucci <npv2@pdx.edu>
2025-02-08 09:45:54 -0800

    Modify ProjectCard CSS to fix spacing when using longer description

--- frontend/app/components/ProjectCard.tsx
+++ frontend/app/components/ProjectCard.tsx
@@ -3,23 +3,27 @@ import { Project } from "../api/projects/route";
 
 export default function ProjectCard({ project }: { project: Project }) {
   return (
-    <div className="bg-[#0d1117] text-white border border-[#3d444d] rounded-lg">
+    <div className="flex flex-col justify-between bg-[#0d1117] text-white border border-[#3d444d] rounded-lg">
       <div className="p-6">
         <h3 className="text-lg font-semibold hover:underline mb-2">
           <Link href={`/projects/${project.id}`}>{project.title}</Link>
         </h3>
-        <p className="text-gray-400 mb-2">{project.description}</p>
-        <a
-          href={project.githubRepoURL}
-          className="text-blue-500 hover:underline"
-        >
-          View on GitHub
-        </a>
+        <p className="text-gray-400">{project.description}</p>
       </div>
-      <div className="flex justify-end bg-[#151b23] border-t border-[#3d444d] rounded-b-lg p-4">
-        <button className="bg-blue-800 hover:bg-blue-900 font-medium rounded-md py-2 px-4">
-          Apply
-        </button>
+      <div>
+        <div className="px-6 pb-4">
+          <a
+            className="text-blue-500 hover:underline"
+            href={project.githubRepoURL}
+          >
+            View on GitHub
+          </a>
+        </div>
+        <div className="flex justify-end bg-[#151b23] border-t border-[#3d444d] rounded-b-lg p-4">
+          <button className="bg-blue-800 hover:bg-blue-900 font-medium rounded-md py-2 px-4">
+            Apply
+          </button>
+        </div>
       </div>
     </div>
   );
