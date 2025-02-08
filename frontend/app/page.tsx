6e679dd99067cd87e20f5a42dcbfc8db6665ca01
Nicholai Venetucci <npv2@pdx.edu>
2025-02-08 00:45:11 -0800

    Remove unused imports and user variable from app/page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,18 +1,11 @@
-import Link from "next/link";
 import { Project } from "./api/projects/route";
-import { getUser } from "./lib/dal";
-import UserHeader from "./components/header/userHeader";
 import ProjectGrid from "./components/ProjectGrid";
 
 const HOST_URL = process.env.HOST_URL;
 
 export default async function Home() {
-    const user = await getUser();
+  const response = await fetch(`${HOST_URL}/api/projects`);
+  const projects: Project[] = await response.json();
 
-    const response = await fetch(`${HOST_URL}/api/projects`);
-    const projects: Project[] = await response.json();
-
-    return (
-        <ProjectGrid projects={projects} />
-    );
+  return <ProjectGrid projects={projects} />;
 }
