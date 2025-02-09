083b58f9f9335255092e9ebc337bf8dc59a2a60e
Logan Brescia <lbrescia@pdx.edu>
2025-02-08 22:00:40 -0800

    Deleted the contents of app/page.tsx to get rid of the double rendering

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,32 +6,5 @@ import UserHeader from "./components/header/userHeader";
 const HOST_URL = process.env.HOST_URL;
 
 export default async function Home() {
-  /*const user = await getUser();
-
-  return (
-    <div className="flex items-center justify-between px-8 py-4">
-      <div className="text-2xl font-bold">Hobby Dev Hub</div>
-      <div>
-        {user ? (
-          <div className="flex items-center">
-            Welcome, {user.data.login}
-            <a
-              href="/api/logout"
-              className="border hover:bg-red-100 border-red-600 text-red-600 py-1 px-4 rounded-md ml-4"
-            >
-              Logout
-            </a>
-          </div>
-        ) : (
-          <a
-            href={`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_APP_CLIENT_ID}`}
-            className="bg-green-600 hover:bg-green-700  text-white py-2 px-4 rounded-md"
-          >
-            Login with Github
-          </a>
-        )}
-      </div>
-    </div>
-  );*/
   return <div></div>;
 }
