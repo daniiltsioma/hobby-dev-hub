6b2dad4d0a523290586d5268a96ffa100895c3b3
Nicholai Venetucci <npv2@pdx.edu>
2025-02-09 02:57:47 -0800

    Remove container div from ProjectCard and add padding 8 to grid div

--- frontend/app/components/ProjectGrid.tsx
+++ frontend/app/components/ProjectGrid.tsx
@@ -3,12 +3,10 @@ import ProjectCard from "./ProjectCard";
 
 export default function ProjectGrid({ projects }: { projects: Project[] }) {
   return (
-    <div className="container mx-auto p-4">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {projects.map((project) => (
-          <ProjectCard project={project} key={project.id} />
-        ))}
-      </div>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-8">
+      {projects.map((project) => (
+        <ProjectCard project={project} key={project.id} />
+      ))}
     </div>
   );
 }
