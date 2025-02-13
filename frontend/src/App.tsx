036478f76dfc2ddd638b5b0aa7f287cef69e30ae
dtsiomapdx <dev.dtsioma@gmail.com>
2025-02-12 22:54:58 -0800

    added basic header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <div>
+            <div className="flex items-center justify-between bg-[#010409] border-b border-[#3d444d] px-8 py-4">
+                <div className="flex items-center">
+                    <div className="text-2xl font-bold">Hobby Dev Hub</div>
+                </div>
+                <div>
+                    <a
+                        href={`https://github.com/login/oauth/authorize?client_id=${
+                            import.meta.env.GITHUB_APP_CLIENT_ID
+                        }`}
+                        className="bg-green-600 hover:bg-green-700 rounded-md py-2 px-4"
+                    >
+                        Login with Github
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
