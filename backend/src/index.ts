343e03d9c30a00db858e1a4a99dadb3812045b61
dtsiomapdx <dev.dtsioma@gmail.com>
2025-02-13 15:53:23 -0800

    added projects API routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,7 @@ import cors from "cors";
 import GithubAPI from "./githubAPI";
 import connectToDatabase from "./mongo/dbConnection";
 import projects from "./mongo/projects";
+import User from "./mongo/models/Users";
 
 const port = process.env.PORT || 8000;
 const frontendUrl = process.env.FRONTEND_HOST_URL || "/";
@@ -69,6 +70,78 @@ app.get("/dummy-db/:id", async (req, res) => {
     const id = Number(req.params.id);
     const project = projects.find((proj) => proj.id === id);
     res.json(project);
+});
+
+app.get("/projects", async (req, res) => {
+    try {
+        await connectToDatabase();
+
+        const { searchParams } = new URL(req.url);
+        const userId = searchParams.get("userId");
+
+        if (!userId) {
+            res.status(400).send("User ID is required");
+        }
+
+        const user = await User.findOne({ userId })
+            .populate("activeProjects")
+            .populate("archivedProjects");
+
+        if (!user) {
+            res.status(404).send("User not found");
+        } else {
+            res.json({
+                activeProjects: user.activeProjects,
+                archivedProjects: user.archivedProjects,
+            });
+        }
+    } catch (error) {
+        console.error("Error fetching projects:", error);
+    }
+});
+
+app.get("/projects/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const project = projects.find((proj) => proj.id === id);
+
+    res.send(project);
+});
+
+app.post("/projects/:id", async (req, res) => {
+    const projectId = Number(req.params.id);
+    const project = projects.find((p) => p.id === projectId);
+
+    if (!project) {
+        res.status(404).send("Project not found");
+        return;
+    }
+
+    // not sure about this block of code
+
+    // const { cookie } = await request.json();
+    // const cookieStore = await cookies();
+    // cookieStore.set({
+    //     name: "accessToken",
+    //     value: cookie.value,
+    //     expires: new Date(Date.now() + Number(cookie.expires_in)),
+    //     httpOnly: true,
+    //     path: "/",
+    // });
+
+    const applicant = await github.getUser();
+
+    if (!applicant || !applicant.data.login) {
+        res.status(401).send("User not authenticated");
+    }
+
+    if (project.applicants.includes(applicant.data.login)) {
+        res.status(400).send("Already applied");
+    }
+
+    if (!project.applicants.includes(applicant.data.login)) {
+        project.applicants.push(applicant.data.login);
+    }
+    res.status(200).json(project);
 });
 
 app.listen(port, () => {
