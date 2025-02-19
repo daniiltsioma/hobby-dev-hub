c115d8330a2fccde77582417d5f33b73bd7ecd08
Logan Brescia <lbrescia@pdx.edu>
2025-02-19 12:48:04 -0800

    Fixed Users schema

--- backend/src/mongo/models/Users.ts
+++ backend/src/mongo/models/Users.ts
@@ -1,21 +1,16 @@
-import mongoose, { Document, Schema } from "mongoose";
+import mongoose, { Schema, Types, model, InferSchemaType } from "mongoose";
 import { IProject } from "./Projects";
-export interface IUser extends Document {
-    email: string;
-    githubId: string;
-    userId: number;
-    activeProjects: IProject[];
-    archivedProjects: IProject[];
-}
 
-const UserSchema: Schema = new mongoose.Schema({
-    email: { type: String, required: true },
-    githubId: { type: String, required: true },
-    userId: { type: Number, required: true },
-    activeProjects: [{ type: Schema.Types.ObjectId, ref: "Project" }],
-    archivedProjects: [{ type: Schema.Types.ObjectId, ref: "Project" }],
+const UserSchema = new Schema({
+  email: { type: String, required: true },
+  githubId: { type: String, required: true },
+  userId: { type: Number, required: true },
+  activeProjects: [{ type: Types.ObjectId, ref: "Project" }],
+  archivedProjects: [{ type: Types.ObjectId, ref: "Project" }],
 });
 
+type IUser = InferSchemaType<typeof UserSchema>;
 const User = mongoose.model<IUser>("User", UserSchema);
 
 export default User;
+export type { IUser };
