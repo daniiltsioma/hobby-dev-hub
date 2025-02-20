fc249131117c1dd7bdf28890951369280eaa4f1e
Logan Brescia <lbrescia@pdx.edu>
2025-02-19 16:38:34 -0800

    implemented the myprojects route so users can view their active and archived projects

--- backend/src/routes/projects.ts
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import projects from "../mongo/models/Projects";
-import User from "../mongo/models/Users";
-import connectToDatabase from "../mongo/dbConnection";
-
-const router = express.Router();
