5ab32a7367c83b81a7319be7ec452407b408d2bd
andrespdx <andg2@pdx.edu>
2025-02-19 16:30:15 -0800

    Able to search for a project

--- frontend/src/components/projects/ProjectSearch.tsx
+++ frontend/src/components/projects/ProjectSearch.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { Project } from "../../../../backend/src/mongo/projects";
+import ProjectCard from "./ProjectCard";
 
 export default function ProjectFilter() {
     const [search, setSearch] = useState("");
@@ -14,7 +15,7 @@ export default function ProjectFilter() {
 
             if (search) queryParams.append("search", search);
             if (tags.length) queryParams.append("tags", tags.join(","));
-    
+
             const response = await fetch(
                 `${import.meta.env.VITE_EXPRESS_URL}/dummy-db-search?${queryParams.toString()}`
             );
@@ -23,14 +24,14 @@ export default function ProjectFilter() {
         }
 
         fetchProjects();
-    }, []);
+    }, [search, tags]);
 
     return (
         <div className="p-4">
             <input
                 type="text"
                 placeholder="Search by name"
-                className="border p-2 border-[#3d444d] rounded w-full mb-4"
+                className="border p-2 border-[#3d444d] focus:outline-none rounded w-full mb-4"
                 value={search}
                 onChange={(e) => setSearch(e.target.value)}
             />
@@ -54,13 +55,11 @@ export default function ProjectFilter() {
 
             <div className="mt-4">
                 {projects.length > 0 ? (
-                    projects.map((project) => (
-                        <div key={project.id} className="border p-4 rounded mb-2">
-                            <h3 className="text-xl font-semibold">{project.title}</h3>
-                            <p>{project.description}</p>
-                            <p><strong>Tags:</strong> {project.technologies.join(", ")}</p>
-                        </div>
-                    ))
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-8">
+                    {projects.map((project) => (
+                        <ProjectCard project={project} key={project.id} />
+                    ))}
+                    </div>
                 ) : (
                     <p>No projects found.</p>
                 )}
