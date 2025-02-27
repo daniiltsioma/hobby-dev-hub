c2f7dbd4b4bec5538b26ee9ca4e3a0c42e8592d0
dtsiomapdx <dev.dtsioma@gmail.com>
2025-02-27 12:33:28 -0800

    added getTokenFromHeader function

--- backend/src/routes/github.ts
+++ backend/src/routes/github.ts
@@ -5,14 +5,33 @@ const githubAPIRouter = Router();
 
 const githubAPI = new GithubAPI();
 
+const getTokenFromHeader = (header: string): string => {
+    const headerParts = header.split(" ");
+    if (headerParts[0] === "Bearer") {
+        return headerParts[1];
+    }
+
+    return "";
+};
+
 githubAPIRouter.post(
     "/github/repos",
     async (req: Request, res: Response): Promise<void> => {
+        if (!req.headers.authorization) {
+            res.status(401).send("Not authorized");
+            return;
+        }
+
         if (!req.body) {
             res.send(null);
         }
 
-        githubAPI.authenticate(req.headers.authorization || "");
+        const authToken = getTokenFromHeader(req.headers.authorization);
+
+        if (!githubAPI.authenticate(authToken)) {
+            res.status(401).send("Failed to authenticate");
+            return;
+        }
 
         const repoData = await githubAPI.createRepo({
             name: req.body.name,
@@ -29,15 +48,20 @@ githubAPIRouter.post(
 githubAPIRouter.post(
     "/github/repos/:repoName/issues",
     async (req: Request, res: Response): Promise<void> => {
+        if (!req.headers.authorization) {
+            res.status(401).send("Not authorized");
+            return;
+        }
+
         if (!req.body) {
             res.status(400).send("No issue data provided.");
+            return;
         }
-        githubAPI.authenticate(req.headers.authorization || "");
 
-        const username = (await githubAPI.getUser())?.login;
+        const authToken = getTokenFromHeader(req.headers.authorization);
 
-        if (!username) {
-            res.status(401).send("Not authenticated");
+        if (!githubAPI.authenticate(authToken)) {
+            res.status(401).send("Failed to authenticate");
         }
 
         try {
