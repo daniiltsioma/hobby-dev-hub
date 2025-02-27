74f65ddffe22ca74cb11a104bd8c15404870d51c
dtsiomapdx <dev.dtsioma@gmail.com>
2025-02-27 11:47:22 -0800

    added create issue API route

--- backend/src/routes/github.ts
+++ backend/src/routes/github.ts
@@ -20,7 +20,36 @@ githubAPIRouter.post(
             description: req.body.description,
         });
 
+        githubAPI.logout();
+
         res.send(repoData);
+    }
+);
+
+githubAPIRouter.post(
+    "/github/repos/:repoName/issues",
+    async (req: Request, res: Response): Promise<void> => {
+        if (!req.body) {
+            res.status(400).send("No issue data provided.");
+        }
+        githubAPI.authenticate(req.headers.authorization || "");
+
+        const username = (await githubAPI.getUser())?.login;
+
+        if (!username) {
+            res.status(401).send("Not authenticated");
+        }
+
+        try {
+            const issueData = await githubAPI.createIssue({
+                repoName: req.params.repoName || "",
+                title: req.body.title,
+                body: req.body.description,
+            });
+            res.status(201).json(issueData);
+        } catch (err) {
+            res.status(400).send(err);
+        }
     }
 );
 
