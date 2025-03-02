7206d8336bcae491b5e559e1d150f1d4a0988372
Nicholai Venetucci <npv2@pdx.edu>
2025-03-02 10:01:39 -0800

    Add project status pills and buttons to inactivate projects

--- frontend/src/routes/MyProjects.tsx
+++ frontend/src/routes/MyProjects.tsx
@@ -37,6 +37,16 @@ export default function MyProjects() {
         fetchProjects();
     }, []);
 
+    // Placeholder action for Inactivating a project
+    const handleInactivate = (projectId: number) => {
+        console.log(`Project with ID: ${projectId} has been inactivated.`);
+    };
+
+    // Placeholder action for Deleting a project
+    const handleDelete = (projectId: number) => {
+        console.log(`Project with ID: ${projectId} has been deleted.`);
+    };
+
     const filterProjects = (
         status: "owned" | "applied" | "collaborating" | "archived"
     ) => {
@@ -105,18 +115,37 @@ export default function MyProjects() {
                                 filterProjects("owned").map((project) => (
                                     <div
                                         key={project.id}
-                                        className="border-b border-[#3d444d] p-4"
+                                        className="border-b border-[#3d444d] p-4 flex"
                                     >
-                                        <h3 className="text-lg font-semibold hover:opacity-80 mb-1">
-                                            <Link
-                                                to={`/projects/${project.id}`}
+                                        <div className="flex-1">
+                                            <h3 className="text-lg font-semibold hover:opacity-80 mb-1">
+                                                <Link
+                                                    to={`/projects/${project.id}`}
+                                                >
+                                                    {project.title}
+                                                </Link>
+                                            </h3>
+                                            <p className="text-[#9198a1]">
+                                                {project.description}
+                                            </p>
+                                        </div>
+
+                                        <div className="flex flex-col justify-between items-end ml-4">
+                                            {/* Status Pill */}
+                                            <span className="text-xs bg-[#2f3742] rounded-full py-1 px-2">
+                                                Owner
+                                            </span>
+
+                                            {/* Archive Button */}
+                                            <button
+                                                onClick={() =>
+                                                    handleInactivate(project.id)
+                                                }
+                                                className="text-sm text-[#e74c3c] hover:text-[#c0392b] cursor-pointer"
                                             >
-                                                {project.title}
-                                            </Link>
-                                        </h3>
-                                        <p className="text-[#9198a1]">
-                                            {project.description}
-                                        </p>
+                                                Archive
+                                            </button>
+                                        </div>
                                     </div>
                                 ))
                             )}
@@ -142,18 +171,37 @@ export default function MyProjects() {
                                 filterProjects("applied").map((project) => (
                                     <div
                                         key={project.id}
-                                        className="border-b border-[#3d444d] p-4"
+                                        className="border-b border-[#3d444d] p-4 flex"
                                     >
-                                        <h3 className="text-lg font-semibold hover:opacity-80 mb-1">
-                                            <Link
-                                                to={`/projects/${project.id}`}
+                                        <div className="flex-1">
+                                            <h3 className="text-lg font-semibold hover:opacity-80 mb-1">
+                                                <Link
+                                                    to={`/projects/${project.id}`}
+                                                >
+                                                    {project.title}
+                                                </Link>
+                                            </h3>
+                                            <p className="text-[#9198a1]">
+                                                {project.description}
+                                            </p>
+                                        </div>
+
+                                        <div className="flex flex-col justify-between items-end ml-4">
+                                            {/* Status Pill */}
+                                            <span className="text-xs bg-[#2f3742] rounded-full py-1 px-2">
+                                                Applicant
+                                            </span>
+
+                                            {/* Withdraw Button */}
+                                            <button
+                                                onClick={() =>
+                                                    handleInactivate(project.id)
+                                                }
+                                                className="text-sm text-[#e74c3c] hover:text-[#c0392b] cursor-pointer"
                                             >
-                                                {project.title}
-                                            </Link>
-                                        </h3>
-                                        <p className="text-[#9198a1]">
-                                            {project.description}
-                                        </p>
+                                                Withdraw
+                                            </button>
+                                        </div>
                                     </div>
                                 ))
                             )}
@@ -181,8 +229,72 @@ export default function MyProjects() {
                                     (project) => (
                                         <div
                                             key={project.id}
-                                            className="border-b border-[#3d444d] p-4"
+                                            className="border-b border-[#3d444d] p-4 flex"
                                         >
+                                            <div className="flex-1">
+                                                <h3 className="text-lg font-semibold hover:opacity-80 mb-1">
+                                                    <Link
+                                                        to={`/projects/${project.id}`}
+                                                    >
+                                                        {project.title}
+                                                    </Link>
+                                                </h3>
+                                                <p className="text-[#9198a1]">
+                                                    {project.description}
+                                                </p>
+                                            </div>
+
+                                            <div className="flex flex-col justify-between items-end ml-4">
+                                                {/* Status Pill */}
+                                                <span className="text-xs bg-[#2f3742] rounded-full py-1 px-2">
+                                                    Collaborator
+                                                </span>
+
+                                                {/* Leave Button */}
+                                                <button
+                                                    onClick={() =>
+                                                        handleInactivate(
+                                                            project.id
+                                                        )
+                                                    }
+                                                    className="text-sm text-[#e74c3c] hover:text-[#c0392b] cursor-pointer"
+                                                >
+                                                    Leave
+                                                </button>
+                                            </div>
+                                        </div>
+                                    )
+                                )
+                            )}
+                        </div>
+                    )}
+                </div>
+
+                {/* Inactive */}
+                <div>
+                    <button
+                        onClick={() => toggleExpand("archived")}
+                        className={`text-xl font-bold w-full text-left bg-[#151b23] hover:bg-[#212830] p-4 ${
+                            expandedSections.includes("archived")
+                                ? "rounded-none" // Not rounded when expanded
+                                : "rounded-b-lg" // Otherwise, apply rounded bottom
+                        }`}
+                    >
+                        Inactive
+                    </button>
+                    {expandedSections.includes("archived") && (
+                        <div className="">
+                            {filterProjects("archived").length === 0 ? (
+                                <p className="text-[#9198a1] border-t border-[#3d444d] p-4">
+                                    You don't have any inactive projects yet.
+                                </p>
+                            ) : (
+                                filterProjects("archived").map((project) => (
+                                    <div
+                                        key={project.id}
+                                        className="border-t border-[#3d444d] p-4 flex"
+                                    >
+                                        <div className="flex-1">
                                             <h3 className="text-lg font-semibold hover:opacity-80 mb-1">
                                                 <Link
                                                     to={`/projects/${project.id}`}
@@ -194,47 +306,23 @@ export default function MyProjects() {
                                                 {project.description}
                                             </p>
                                         </div>
-                                    )
-                                )
-                            )}
-                        </div>
-                    )}
-                </div>
 
-                {/* Archived */}
-                <div>
-                    <button
-                        onClick={() => toggleExpand("archived")}
-                        className={`text-xl font-bold w-full text-left bg-[#151b23] hover:bg-[#212830] p-4 ${
-                            expandedSections.includes("archived")
-                                ? "rounded-none" // Not rounded when expanded
-                                : "rounded-b-lg" // Otherwise, apply rounded bottom
-                        }`}
-                    >
-                        Archived
-                    </button>
-                    {expandedSections.includes("archived") && (
-                        <div className="">
-                            {filterProjects("archived").length === 0 ? (
-                                <p className="text-[#9198a1] border-t border-[#3d444d] p-4">
-                                    You don't have any archived projects yet.
-                                </p>
-                            ) : (
-                                filterProjects("archived").map((project) => (
-                                    <div
-                                        key={project.id}
-                                        className="border-t border-[#3d444d] p-4"
-                                    >
-                                        <h3 className="text-lg font-semibold hover:opacity-80 mb-1">
-                                            <Link
-                                                to={`/projects/${project.id}`}
+                                        <div className="flex flex-col justify-between items-end ml-4">
+                                            {/* Status Pill */}
+                                            <span className="text-xs bg-[#2f3742] rounded-full py-1 px-2">
+                                                Owner/Applicant/Collaborator
+                                            </span>
+
+                                            {/* Delete Button */}
+                                            <button
+                                                onClick={() =>
+                                                    handleDelete(project.id)
+                                                }
+                                                className="text-sm text-[#e74c3c] hover:text-[#c0392b] cursor-pointer"
                                             >
-                                                {project.title}
-                                            </Link>
-                                        </h3>
-                                        <p className="text-[#9198a1]">
-                                            {project.description}
-                                        </p>
+                                                Delete
+                                            </button>
+                                        </div>
                                     </div>
                                 ))
                             )}
