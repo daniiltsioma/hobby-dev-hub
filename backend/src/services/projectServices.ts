083cf5ba70004b0948f91efc408b272589ea8ee4
Logan Brescia <lbrescia@pdx.edu>
2025-03-07 20:33:09 -0800

    Changed the schema to better fit, I've also updated and added a lot of project services

--- backend/src/services/projectServices.ts
+++ backend/src/services/projectServices.ts
@@ -7,37 +7,38 @@ import { IUser } from "../mongo/models/Users";
 // TODO: IGNORE CASE OF NAME
 export default class projectServices {
   async createProject(projectData: {
-    name: string;
-    repoUrl: string;
+    title: string;
+    githubRepoURL: string;
     description?: string;
-    tags: string[];
-    owner: IUser;
-    sprintStatus?: string;
-    approvedUsers?: string;
+    technologies: string[];
+    owner: string;
+    applicants?: string[];
+    collaborators?: string;
     tasks?: string[];
     startDate?: Date;
     endDate?: Date;
+    isArchived?: boolean;
   }) {
     try {
       await connectToDatabase();
 
       if (
-        !projectData.name ||
-        !projectData.repoUrl ||
-        !projectData.tags ||
+        !projectData.title ||
+        !projectData.githubRepoURL ||
+        !projectData.technologies ||
         !projectData.owner
       ) {
         throw new Error("Some required fields are missing");
       }
 
-      const existingProject = await Project.findOne(
-        { name: projectData.name },
-        { owner: projectData.owner }
-      );
+      const existingProject = await Project.findOne({
+        title: { $regex: new RegExp(`^${projectData.title}$`, "i") },
+        owner: projectData.owner,
+      });
+
       if (existingProject) {
-        const githubId = projectData.owner.githubId;
         throw new Error(
-          `A project for user '${githubId}' with the name '${projectData.name}' already exists.`
+          `A project for user '${projectData.owner}' with the title '${projectData.title}' already exists.`
         );
       }
 
@@ -45,13 +46,82 @@ export default class projectServices {
       return await newProject.save();
     } catch (error) {
       console.error(
-        "Error creating the new project '" + projectData.name + "'",
+        "Error creating the new project '" + projectData.title + "'",
         error
       );
       throw error;
     }
   }
 
+  // return all projects for a specific owner
+  async returnAllProjectsForAnOwner(username: string) {
+    try {
+      await connectToDatabase();
+
+      if (!username) {
+        throw new Error("No username provided, cannot fetch the projects");
+      }
+
+      const projects = await Project.find({
+        $or: [
+          { owner: username },
+          { collaborators: username },
+          { applicants: username },
+        ],
+      });
+
+      return projects;
+    } catch (error) {
+      console.error(`Error fetching projects for user '${username}'`, error);
+      throw error;
+    }
+  }
+
+  // get projects based on user role
+  async getProjectsByUserRole(
+    username: string,
+    role: "owned" | "applied" | "collaborating" | "archived"
+  ) {
+    try {
+      await connectToDatabase();
+
+      let query = {};
+      switch (role) {
+        case "owned":
+          query = {
+            owner: { $regex: new RegExp(`^${username}$`, "i") },
+            isArchived: false,
+          };
+          break;
+        case "collaborating":
+          query = {
+            collaborators: { $regex: new RegExp(`^${username}$`, "i") },
+            owner: { $ne: { $regex: new RegExp(`^${username}$`, "i") } },
+            isArchived: false,
+          };
+          break;
+        case "archived":
+          query = {
+            isArchived: true,
+            $or: [
+              { owner: { $regex: new RegExp(`^${username}$`, "i") } },
+              { collaborators: { $regex: new RegExp(`^${username}$`, "i") } },
+            ],
+          };
+          break;
+        default:
+          throw new Error("Invalid role specified.");
+      }
+
+      const projects = await Project.find(query);
+      return projects;
+    } catch (error) {
+      console.error(`Error fetching projects for role '${role}'`, error);
+      throw error;
+    }
+  }
+
+  // get a single project
   async getProject(name: string) {
     try {
       await connectToDatabase();
@@ -75,6 +145,67 @@ export default class projectServices {
       console.error("Error fetching project '" + name + "'", error);
       throw error;
     }
+  }
+
+  async addCollaborator(
+    title: string,
+    owner: string,
+    userToAddAsCollaborator: string
+  ) {
+    try {
+      await connectToDatabase();
+
+      if (!title || !owner || !userToAddAsCollaborator) {
+        throw new Error("One or more required arguments is empty");
+      }
+      const project = await Project.findOne(
+        { $regex: new RegExp(`^${title}$`, "i") },
+        { $regex: new RegExp(`^${owner}$`, "i") }
+      );
+      if (!project) {
+        throw new Error("Project not found.");
+      }
+
+      if (!project.applicants.includes(userToAddAsCollaborator)) {
+        project.applicants.push(userToAddAsCollaborator);
+        await project.save();
+      }
+
+      return project;
+    } catch (error) {
+      console.error(
+        "Error adding applicant '" + userToAddAsCollaborator + "'",
+        error
+      );
+      throw error;
+    }
+  }
+
+  async removeCollaborator(title: string, owner: string, userToRemove: string) {
+    try {
+      await connectToDatabase();
+
+      if (!title || !owner || !userToRemove) {
+        throw new Error("One or more required arguments is empty");
+      }
+
+      const project = await Project.findOne(
+        { $regex: new RegExp(`^${title}$`, "i") },
+        { $regex: new RegExp(`^${owner}$`, "i") }
+      );
+
+      if (!project) {
+        throw new Error("Project not found");
+      }
+
+      project.collaborators = project.collaborators.filter(
+        (collab) => collab.toLowerCase() !== userToRemove.toLowerCase()
+      );
+
+      await project.save();
+
+      return project;
+    } catch (error) {}
   }
 
   async updateProject(name: string, updateData: Partial<typeof Project>) {
